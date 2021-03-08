8b43f83dcbe55789e8992c9e6201831063cad09a
Catherinekarali <41665518+Catherinekarali@users.noreply.github.com>
2021-03-09 01:08:09 +0200

    LLC_prefetcher_files

--- prefetcher/secondpred.py
+++ prefetcher/secondpred.py
@@ -4,21 +4,21 @@
 #import sys
 #sys.path.append('~/Documents/ChampSim/prefetcher')
 #import tensorflow as tf
-from tensorflow.keras.backend import clear_session
 #tf.device("cpu:0")
+from tensorflow.keras.backend import clear_session
 from numpy import array
 from keras.models import model_from_json
-#rom keras.models import load_model
+#from keras.models import load_model
 from keras import backend as K
 
 def pref_init():
-	Model = '644ipcp_33_cells32'
+	Model = '649_33_cells32'
 	global model
 	K.clear_session()
 #	model=load_model(Model+"new")
 #	tf.keras.backend.clear_session()
-	clear_session()
 #	model = tf.keras.models.load_model(Model+"new", compile=False)
+	clear_session()
 	json_file = open(Model + '.json', 'r')
 	loaded_model_json = json_file.read()
 	json_file.close()
@@ -38,4 +38,6 @@ def pref_operate(h_list):
 
 	a = model.predict(XX)
 	pred1 = list(a[0]).index(max(list(a[0])))-64
-	return (pred1,)
+	a[0][list(a[0]).index(max(list(a[0])))] = 0
+	pred2 = list(a[0]).index(max(list(a[0])))-64
+	return (pred1, pred2) 
